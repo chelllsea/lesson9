287bbe9a89b4ebcf8035bd36a0dbcbb35684bae7
chelllsea <44092576+chelllsea@users.noreply.github.com>
2019-01-16 14:32:27 -0500

    Add files via upload

---
+++ Lesson 9/alienbot.py
@@ -0,0 +1,15 @@
+print('-' * 50)
+
+print('Alien Greeter Bot: ')
+
+planet = input('What planet are you from? ')
+
+if planet == 'Earth':
+	print("Yikes no wonder.")
+	print("Whatever, have a great day i guess")
+
+else:
+	print('Welcome to Earth!')
+	print("Please don't kill us.")
+
+print('-' * 50)
